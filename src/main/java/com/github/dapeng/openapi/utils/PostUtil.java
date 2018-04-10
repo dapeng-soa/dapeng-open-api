c2f14c568a0c8af98ec65e3db46f92086c2e24f3
StruggleYang <yq1724555319@gmail.com>
2018-04-11 01:50:34 +0800

    update 2.0.1

--- src/main/java/com/github/dapeng/openapi/utils/PostUtil.java
+++ src/main/java/com/github/dapeng/openapi/utils/PostUtil.java
@@ -3,7 +3,9 @@ package com.github.dapeng.openapi.utils;
 import com.github.dapeng.client.netty.JsonPost;
 import com.github.dapeng.core.InvocationContext;
 import com.github.dapeng.core.InvocationContextImpl;
+import com.github.dapeng.core.SoaCode;
 import com.github.dapeng.core.SoaException;
+import com.github.dapeng.core.enums.CodecProtocol;
 import com.github.dapeng.core.metadata.Service;
 import com.github.dapeng.openapi.cache.ServiceCache;
 import org.slf4j.Logger;
@@ -18,6 +20,7 @@ import java.util.Set;
  */
 public class PostUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(PostUtil.class);
+    private static final String OPEN_API_TIMEOUT = "soa.service.timeout";
 
     public static String post(String service,
                               String version,
@@ -29,15 +32,25 @@ public class PostUtil {
         invocationCtx.setVersionName(version);
         invocationCtx.setMethodName(method);
         invocationCtx.setCallerFrom(Optional.of("JsonCaller"));
-        //设置请求超时时间 10 s
-        invocationCtx.setTimeout(Optional.of(120000L));
-        LOGGER.info("<=========>   timeout:" + invocationCtx.getTimeout());
+        if (!invocationCtx.getTimeout().isPresent()) {
+            //设置请求超时时间,从环境变量获取，默认 10s ,即 10000
+            Long timeOut = Long.valueOf(getEnvTimeOut());
+            invocationCtx.setTimeout(Optional.of(timeOut));
+        }
+
+
+        invocationCtx.setCodecProtocol(CodecProtocol.CompressedBinary);
 
         Service bizService = ServiceCache.getService(service, version);
 
+        if (bizService == null) {
+            LOGGER.error("bizService not found[service:" + service + ", version:" + version + "]");
+            return String.format("{\"responseCode\":\"%s\", \"responseMsg\":\"%s\", \"success\":\"%s\", \"status\":0}", SoaCode.NotMatchedService.getCode(), SoaCode.NotMatchedService.getMsg(), "{}");
+        }
+
         fillInvocationCtx(invocationCtx, req);
 
-        JsonPost jsonPost = new JsonPost(service, method, true);
+        JsonPost jsonPost = new JsonPost(service, version, true);
 
         try {
             return jsonPost.callServiceMethod(invocationCtx, parameter, bizService);
@@ -84,5 +97,17 @@ public class PostUtil {
         if (parameters.contains("operatorId")) {
             invocationCtx.setOperatorId(Optional.of(Integer.valueOf(req.getParameter("operatorId"))));
         }
+    }
+
+
+    private static String getEnvTimeOut() {
+        String timeOut = System.getenv(OPEN_API_TIMEOUT.replaceAll("\\.", "_"));
+        if (timeOut == null) {
+            timeOut = System.getProperty(OPEN_API_TIMEOUT);
+        }
+        if (timeOut == null) {
+            timeOut = "10000";
+        }
+        return timeOut;
     }
 }
